a813badec0078735ebaad69efc49c18d7d8bba14
noelsaldanha1991 <nl.saldanha@gmail.com>
2024-07-06 20:16:55 -0400

    2nd commit

--- src/main/java/com/javatechie/aws/lambda/OrderHandler.java
+++
@@ -1,6 +0,0 @@
-package com.javatechie.aws.lambda;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class OrderHandler extends SpringBootRequestHandler<String,Object> {
-}
---
+++ src/main/java/com/javatechie/aws/lambda/SpringbootAwsApplication.java
@@ -0,0 +1,32 @@
+package com.javatechie.aws.lambda;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatechie.aws.lambda.domain.Order;
+import com.javatechie.aws.lambda.respository.OrderDao;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/orders")
+public class SpringbootAwsApplication {
+
+	@Autowired
+	OrderDao orderDao;
+
+	@GetMapping
+	public List<Order> getBooks() {
+		return orderDao.buildOrders();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsApplication.class, args);
+	}
+
+}
